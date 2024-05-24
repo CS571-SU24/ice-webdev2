ab64c59cdb6fc4af849514ce72f973d0d6704307
Cole Nelson <ctnelson1997@gmail.com>
2024-05-24 11:40:14 -0500

    mv logging

--- solution/src/components/Recipe.jsx
+++ solution/src/components/Recipe.jsx
@@ -10,25 +10,6 @@ export default function Recipe(props) {
         setLikes(oLikes => oLikes + 1)
     }
 
-    useEffect(() => {
-        console.log(Stopwatch.get(), `Recipe component has mounted for ${props.name ?? "anonymous"} recipe!`)
-
-        return () => {
-            console.log(Stopwatch.get(), `Recipe component has unmounted for ${props.name ?? "anonymous"} recipe!`)
-        }
-    }, []);
-
-    // useEffect(() => {
-    //     console.log(Stopwatch.get(), `The number of likes has changed for ${props.name ?? "anonymous"} recipe!`)
-
-    //     return () => {
-    //         console.log(Stopwatch.get(), `The number of likes is about to change for ${props.name ?? "anonymous"} recipe!`)
-    //     }
-    // }, [likes]);
-
-    console.log(Stopwatch.get(), `${props.name ?? "anonymous"} Recipe is rendering!`)
-
-
     return <Card style={{margin: "auto", marginTop: "1rem", maxWidth: "40rem"}}>
         {
             Object.keys(props).length > 0 ? <>
--- starter/src/components/Recipe.jsx
+++ starter/src/components/Recipe.jsx
@@ -10,6 +10,24 @@ export default function Recipe(props) {
         setLikes(oLikes => oLikes + 1)
     }
 
+    useEffect(() => {
+        console.log(Stopwatch.get(), `Recipe component has mounted for ${props.name ?? "anonymous"} recipe!`)
+
+        return () => {
+            console.log(Stopwatch.get(), `Recipe component has unmounted for ${props.name ?? "anonymous"} recipe!`)
+        }
+    }, []);
+
+    // useEffect(() => {
+    //     console.log(Stopwatch.get(), `The number of likes has changed for ${props.name ?? "anonymous"} recipe!`)
+
+    //     return () => {
+    //         console.log(Stopwatch.get(), `The number of likes is about to change for ${props.name ?? "anonymous"} recipe!`)
+    //     }
+    // }, [likes]);
+
+    console.log(Stopwatch.get(), `${props.name ?? "anonymous"} Recipe is rendering!`)
+
     return <Card style={{margin: "auto", marginTop: "1rem"}}>
         <img src={props.img.location} alt={props.img.description}/>
         <h2>{props.name}</h2>
